fb40108e57ad9c878cda01bc96880b3d9a5827bf
Muhammed-Mert <75245988+Muhammed-Mert@users.noreply.github.com>
2024-11-17 15:42:59 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,1 +1,1 @@
-yay -S --needed vlc drawing brave-bin spotify anydesk-bin remmina rustdesk-bin bleachbit okular libreoffice-still gparted discord whatsapp-for-linux visual-studio-code-bin steam heroic-games-launcher-bin wine-stable  zen-browser-bin guilded proton-vpn-gtk-app obs-studio-git
+yay -S --needed drawing brave-bin spotify anydesk-bin remmina rustdesk-bin bleachbit okular libreoffice-still gparted discord whatsapp-for-linux visual-studio-code-bin steam heroic-games-launcher-bin wine-stable  zen-browser-bin guilded proton-vpn-gtk-app obs-studio-git
