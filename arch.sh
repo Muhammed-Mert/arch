250de7f023dc71afbb44c138629f67999912ba05
Muhammed-Mert <75245988+Muhammed-Mert@users.noreply.github.com>
2025-03-14 17:56:17 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,1 +1,10 @@
+sudo pacman -Syy
+sudo pacman -Syu
+cd
+mkdir git
+cd git
+sudo pacman -S git neofetch
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
 yay -S --needed drawing brave-bin spotify anydesk-bin bleachbit libreoffice-still gparted discord steam heroic-games-launcher-bin wine-stable librewolf obs-studio-git spectacle gwenview
