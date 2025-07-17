862b2c0be92e5e3bec32885fc1fbed3184a8212e
Muhammed-Mert <75245988+Muhammed-Mert@users.noreply.github.com>
2025-07-17 13:19:28 +0300

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -3,8 +3,8 @@ sudo pacman -Syu
 cd
 mkdir git
 cd git
-sudo pacman -S git neofetch
+sudo pacman -S git neofetch ntfs-3g vlc vlc-plugins-all flatpak
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S --needed drawing brave-bin spotify anydesk-bin bleachbit libreoffice-still gparted discord steam heroic-games-launcher-bin wine-stable librewolf obs-studio spectacle gwenview distrobox boxbuddy waydroid
+yay -S --needed drawing brave-bin spotify bleachbit libreoffice-still gparted discord steam protontricks winetricks heroic-games-launcher-bin lutris librewolf obs-studio spectacle gwenview distrobox boxbuddy legacy-launcher protonplus wine-stable waydroid anydesk-bin 
